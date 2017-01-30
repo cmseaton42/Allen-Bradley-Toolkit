c1b2f0f2b4cd85ec7348ed437a834436c2c08853
root <root@localhost.localdomain>
2017-01-30 16:55:03 -0600

    Added Project and Controller Templates [UNTESTED]

--- src/lib/PIA/Rockwell/XML/Templates.py
+++ src/lib/PIA/Rockwell/XML/Templates.py
@@ -6,6 +6,111 @@ except ImportError as e:
 
 from Tools import *
 
+class Project():
+    '''
+    Project Template:
+    See L5X Manual for Details,
+    These members are to be used when defining a datatype.
+    ----------------------------------------------------------
+    For Information on this see the provided L5X Manual from Rockwell
+    '''
+    def __init__(self, SchemaRevision = "1.0", SoftwareRevision = "30.00"):
+            #Initialize Member Attributes
+            self.root = etree.Element("RSLogix5000Content")
+
+    def getControllerRoot(self):
+        return self.Controller
+
+    def addController(self, Controller):
+        assert etree.iselement(Controller.getLocalRoot()) and Datatype.getLocalRoot().tag = "Controller"
+        if not self.checkIfChild("Controller"):
+            self.Controller = etree.SubElement(self.root, "Controller")
+        self.Controller.append(Datatype)
+
+    def setAttribute(self, **kwargs):
+        for key in kwargs:
+            self.root.set(key, kwargs[key])
+
+    def getLocalRoot(self):
+        return self.root
+
+    def __str__(self):
+        return etree.tostring(self.root, pretty_print = True)
+
+class Controller():
+    '''
+    Controller Template:
+    See L5X Manual for Details,
+    These members are to be used when defining a datatype.
+    ----------------------------------------------------------
+    For Information on this see the provided L5X Manual from Rockwell
+    '''
+    def __init__(self, Name, Use = "Context", Description = ""):
+            #Initialize Member Attributes
+            assert isValidTag(TagName)
+            self.root = etree.Element('Controller')
+            if Description != "":
+                self.Desc = etree.SubElement(self.root, 'Description')
+                self.setDescription(Description)
+                self.root.append(self.Desc)
+
+    def checkIfChild(self, nodeTag):
+        assert type(nodeTag) == str
+        for node in self.root:
+            if node.tag == NodeTag: return False
+        return True
+
+    def addDatatype(self, Datatype):
+        assert etree.iselement(Datatype.getLocalRoot()) and Datatype.getLocalRoot().tag = "Datatype"
+        if not self.checkIfChild("Datatypes"):
+            self.Datatypes = etree.SubElement(self.root, "Datatypes")
+        self.Datatypes.append(Datatype)
+
+    def addModule(self, Module):
+        assert etree.iselement(Module.getLocalRoot()) and Datatype.getLocalRoot().tag = "Module"
+        if not self.checkIfChild("Modules"):
+            self.Modules = etree.SubElement(self.root, "Modules")
+        self.Modules.append(Datatype)
+
+    def addAddOnInstructionDefinition(self, AddOnInstructionDefinition):
+        assert etree.iselement(AddOnInstructionDefinition.getLocalRoot()) and Datatype.getLocalRoot().tag = "AddOnInstructionDefinition"
+        if not self.checkIfChild("AddOnInstructionDefinitions"):
+            self.AddOnInstructionDefinitions = etree.SubElement(self.root, "AddOnInstructionDefinitions")
+        self.AddOnInstructionDefinitions.append(Datatype)
+
+    def addProgram(self, Program):
+        assert etree.iselement(Program.getLocalRoot()) and Datatype.getLocalRoot().tag = "Program"
+        if not self.checkIfChild("Programs"):
+            self.Programs = etree.SubElement(self.root, "Programs")
+        self.Programs.append(Datatype)
+
+    def addTask(self, Task):
+        assert etree.iselement(Task.getLocalRoot()) and Datatype.getLocalRoot().tag = "Task"
+        if not self.checkIfChild("Tasks"):
+            self.Tasks = etree.SubElement(self.root, "Tasks")
+        self.Tasks.append(Datatype)
+
+    def setAttribute(self, **kwargs):
+        for key in kwargs:
+            self.root.set(key, kwargs[key])
+
+    def setDescription(self, Description):
+        assert type(Description) == str
+        if self.Desc == None:
+            self.Desc = etree.SubElement(self.root, 'Description')
+            self.root.append(self.Desc)
+        self.Desc.text = etree.CDATA(Description)
+
+    def setParent(self, root):
+        assert etree.iselement(root) and root.tag == "Members"
+        root.append(self.getLocalRoot())
+
+    def getLocalRoot(self):
+        return self.root
+
+    def __str__(self):
+        return etree.tostring(self.root, pretty_print = True)
+
 class Member():
     '''
     Member Template:
