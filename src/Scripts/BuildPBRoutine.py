37a49794981fc7c9c0c67a3c8eb4e79afd4d9638
root <root@localhost.localdomain>
2017-02-07 08:26:56 -0600

    Small Change...

--- src/Scripts/BuildPBRoutine.py
+++ src/Scripts/BuildPBRoutine.py
@@ -221,22 +221,19 @@ except:
     pass
 shutil.copy(PATH, DIR)
 
-if set_reg(r"wallpaper", KEY_VALUE):
-    print "Done"
-else: print "Fail"
+if set_reg(r"wallpaper", KEY_VALUE): print "-*-"
 
-print get_reg(r"wallpaper")
 
 def set_reg(name, value):
-    #try:
+    try:
         _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, REG_PATH)
         registry_key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, REG_PATH, 0,
                                        _winreg.KEY_WRITE)
         _winreg.SetValueEx(registry_key, name, 0, _winreg.REG_SZ, value)
         _winreg.CloseKey(registry_key)
         return True
-    #except WindowsError:
-        #return False
+    except WindowsError:
+        return False
 
 def get_reg(name):
     try:
