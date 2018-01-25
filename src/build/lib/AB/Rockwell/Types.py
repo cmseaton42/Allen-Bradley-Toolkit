2147fd4b4fe990298e7a475328147f40fe9cca0d
Canaan Seaton <cseaton@bastiansolutions.com>
2018-01-25 12:26:20 -0500

    another commit

---
+++ src/build/lib/AB/Rockwell/Types.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class CommonType:
+    BOOL = "BOOL"
+    SINT = "SINT"
+    INT  = "INT"
+    DINT = "DINT"
+    REAL = "REAL"
+    STRING = "STRING"
+    COUNTER = "COUNTER"
+    TIMER = "TIMER"
+
+class UserDefinedTypes:
+    ALARM = "ST_Alarms"
+    ONE_BUTTON  = "PB_OneButton"
+    TWO_BUTTON  = "PB_TwoButton"
---
+++ src/build/lib/AB/Rockwell/XML/Templates/__init__.py
@@ -0,0 +1,11 @@
+__all__ = ["Controller", "Datatype", "Member", "Program", "Project", "Routine", "Rung", "Tag", "TagComment"]
+
+from Controller import Controller
+from Datatype import Datatype
+from Member import Member
+from Program import Program
+from Project import Project
+from Routine import Routine
+from Rung import Rung
+from Tag import Tag
+from TagComment import TagComment
